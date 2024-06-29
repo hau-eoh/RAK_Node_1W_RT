140703d0287d49015dadec1bbec5bca72489556d
HaQuocAn2406 <quocan24062002@gmail.com>
2024-06-30 00:20:00 +0700

    Sleep Mode--ok

--- RAK_Node_1W_RT_v1/RAK_Node_1W_RT_v1.ino
+++ RAK_Node_1W_RT_v1/RAK_Node_1W_RT_v1.ino
@@ -2,13 +2,14 @@
 #include "Canopus_Modbus.h"
 #include "XY_MD02.h"
 #include "ds18b20.h"
-
+#define button_pin PA11
 double myFreq = 922500000;
 uint16_t sf = 12, bw = 0, cr = 0, preamble = 8, txPower = 22;
 
 
 uint8_t MAC[6] = { 0x01, 0x02, 0x03, 0x04, 0x06, 0x06 };
 uint8_t frame[16];
+uint8_t count=0;
 bool rx_done = false;
 bool join_success = false;
 bool gateway_ok = false;
@@ -32,6 +33,8 @@ void setup() {
   enable_Vrs485();  // Init cổng rs485
   init_xymd02();    // init XY_MD02
   startTime = millis();
+  pinMode(button_pin,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(button_pin), ButtonHandle, CHANGE);
   if (api.lora.nwm.get() != 0)  // Set P2P mode
   {
     Serial.printf("Set Node device work mode %s\r\n", api.lora.nwm.set() ? "Success" : "Fail");
@@ -51,25 +54,28 @@ void setup() {
   Serial.printf("Set P2P mode tx power %d: %s\r\n", txPower, api.lora.ptp.set(txPower) ? "Success" : "Fail");              //set TX power
   Serial.printf("Set the low power mode %s\n\r", api.system.lpm.set(1) ? "Success" : "Fail");
   api.lora.registerPRecvCallback(recv_cb);  // goij hàm recv_cb mỗi khi nhận dữ liệu xong
-  api.lora.registerPSendCallback(send_cb);
-  // gọi hàm send_cb mỗi khi gửi xong
-    while (!join_success) {
-    uint8_t join_data[1] = { 0x01 };       // node dùng cảm biến rời + onewire
-    send_frame(0x01, 0x01, join_data, 1);  // gửi frame request join lên gateway
-    Serial.println("Join request Sent");
-    // api.lora.precv(5000);
-    delay(6000);
-    if(join_success){
-      Serial.println("-----------------------               --------------------");
-      Serial.println("----------------------- Join Success --------------------");
-      Serial.println("-----------------------               --------------------");
-    }else{
-      Serial.println("-----------------------               --------------------");
-      Serial.println("----------------------- !!!! JOIN FAIL !!!! --------------------");
-      Serial.println("-----------------------               --------------------");
-    }
+  api.lora.registerPSendCallback(send_cb);// gọi hàm send_cb mỗi khi gửi xong
+  for(int i=0;i<20;i++){
+    digitalWrite(LED_SYNC,!digitalRead(LED_SYNC));
+    delay(50);
   }
-  Serial.println("Sleeping............");
+  digitalWrite(LED_SYNC,1);
+  // while (!join_success) {
+  //   uint8_t join_data[1] = { 0x01 };       // node dùng cảm biến rời + onewire
+  //   send_frame(0x01, 0x01, join_data, 1);  // gửi frame request join lên gateway
+  //   Serial.println("Join request Sent");
+  //   // api.lora.precv(5000);
+  //   delay(6000);
+  //   if(join_success){
+  //     Serial.println("-----------------------               --------------------");
+  //     Serial.println("----------------------- Join Success --------------------");
+  //     Serial.println("-----------------------               --------------------");
+  //   }else{
+  //     Serial.println("-----------------------               --------------------");
+  //     Serial.println("------------------- !!! JOIN FAIL !!!! -------------------");
+  //     Serial.println("-----------------------               --------------------");
+  //   }
+  // }
   if (api.system.timer.create(RAK_TIMER_0, (RAK_TIMER_HANDLER)WakeUp, RAK_TIMER_PERIODIC) != true) {
     Serial.printf("Creating timer failed.\r\n");
   } 
@@ -79,8 +85,7 @@ void setup() {
 }
 
 void loop() {
-  api.system.sleep.all();
-  api.system.scheduler.task.destroy();
+  sleep_mode();
 }
 /*////////////////////////////////////////////////
 
@@ -89,27 +94,51 @@ void loop() {
 ///////////////////////////////////////////////////*/
 void WakeUp()
 {   
-    // udrv_sys_clock_on();
     Serial.println("Hàm Callback ");
-    for(int i=0;i<10;i++){
-      digitalWrite(LED_RECV,!digitalRead(LED_RECV));
-      delay(300);
+    count++;
+    if(count==1440){// 24h sẽ gửi pin
+      float battery = (api.system.bat.get()/4.2)*100;
+      uint8_t batt_int = battery;
+      Serial.printf("Battery: %d%% ",batt_int);
+      send_frame(0x03, 0x07, &batt_int, 1);
+      delay(2000);
+      count=0;
     }
     send_data();
 }
-
+void ButtonHandle(){
+  if(digitalRead(button_pin)==LOW){
+    long pre=0;
+    if(millis()-pre>=200){
+      pre=millis();
+      if(digitalRead(button_pin)==LOW){
+        Serial.println("_______________________________  External Interrupt!!!_________________________________________");
+        uint8_t status = 0;
+        status=!status;
+        // for(int i=0;i<10;i++){
+        //   digitalWrite(LED_SYNC,!digitalRead(LED_SYNC));
+        //   delay(300);
+        // }
+          uint8_t adam_data[4];
+          adam_data[0] = status;
+          adam_data[1] = 0;
+          adam_data[2] = 0;
+          adam_data[3] = 0;
+          send_frame(0x03, 0x05, adam_data, 4);
+      }else{
+      return;
+      }
+    }
+  }
+}
 void send_data(){
-  float battery = (api.system.bat.get()/4.2)*100;
-  uint8_t batt_int = battery;
   float temp = read_temperature();           // biến nhiệt độ từ 1W 16bit
   uint16_t temp_scaled = temp * 10;          //123
   uint8_t temp_data[2];                   // mảng tách giá trị của nhiệt độ từ 1W
-  Serial.printf("Battery: %d%% ",batt_int);
   Serial.print("scaled temp: ");
   Serial.println(temp_scaled);
   Serial.print("temp: ");
   Serial.println(temp);
-  
   temp_data[0] = temp_scaled & 0xFF;         // LSB//8bit cao
   temp_data[1] = (temp_scaled >> 8) & 0xFF;  // MSB//8 bit thấp
   uint16_t xy_md02_temp = read_temp();       // biến nhiệt độ từ rs485 16bit
@@ -130,8 +159,6 @@ void send_data(){
   send_frame(0x03, 0x03, tempmd02_data, 2);  // gửi frame nhiệt độ từ rs485
   delay(2000);                               // Chờ cho gateway nhận dữ liệu
   send_frame(0x03, 0x04, temp_data, 2);
-  delay(2000);
-  send_frame(0x03, 0x07, &batt_int, 1);
 }
 void hexDump(uint8_t *buf, uint16_t len) {
   for (uint16_t i = 0; i < len; i += 16) {  //Duyet qua buffer theo tung nhom 2byte
@@ -154,11 +181,16 @@ void hexDump(uint8_t *buf, uint16_t len) {
 }
 void recv_cb(rui_lora_p2p_recv_t data) {
   rx_done = true;
-  if (data.BufferSize == 0) {
-    Serial.println("Empty buffer.");
+  if(data.Status==1){
+    digitalWrite(LED_RECV,0);
+    Serial.println("________________ Recv Nothing ____________");
+    digitalWrite(LED_RECV,0);
+    sleep_mode();
     return;
   }
-  digitalWrite(LED_RECV, !digitalRead(LED_RECV));
+  // else if(data.BufferSize == 0) {
+  //   Serial.println("Empty buffer.");
+  // }
   char buff[92];
   uint8_t respone = buff[5];
   if (respone == 0x00 || 0x01) {
@@ -168,11 +200,14 @@ void recv_cb(rui_lora_p2p_recv_t data) {
   }
   sprintf(buff, "Incoming message, length: %d, RSSI: %d, SNR: %d", data.BufferSize, data.Rssi, data.Snr);
   Serial.println(buff);
+  digitalWrite(LED_RECV,0);
   // hexDump(data.Buffer, data.BufferSize);
-  digitalWrite(LED_RECV, !digitalRead(LED_RECV));
+  // sleep_mode();
 }
 void send_cb(void) {
-  Serial.printf("P2P Set RX Mode: %s\r\n", api.lora.precv(65534) ? "Success" : "Fail");  // bật mode RX, TX vẫn có thể hoạt động được
+  digitalWrite(LED_RECV,1);
+  digitalWrite(LED_SEND,0);
+  Serial.printf("P2P Set RX Mode: %s\r\n", api.lora.precv(20000) ? "Success" : "Fail");  // bật mode RX 20s
 }
 uint8_t crc8(const uint8_t *data, int len) {
   unsigned crc = 0;
@@ -189,6 +224,7 @@ uint8_t crc8(const uint8_t *data, int len) {
   return (uint8_t)(crc >> 8);
 }
 void send_frame(uint8_t frame_type, uint8_t function, uint8_t *data, uint8_t data_len) {
+  digitalWrite(LED_SEND,1);
   bool send_result = false;
   len = 0;                              //must have for reset len each call function
   frame[len++] = 0xFE;                  // BEGIN
@@ -229,3 +265,12 @@ void send_frame(uint8_t frame_type, uint8_t function, uint8_t *data, uint8_t dat
   Serial.println();
   // Serial.println("Waiting for gateway respone............");
 }
+void sleep_mode(){
+  Serial.println("________________  Sleeping ____________________-");
+  digitalWrite(LED_SYNC,0);
+  if(!digitalRead(LED_SYNC)){
+    digitalWrite(LED_SYNC,0);
+  }
+  api.system.sleep.all();
+  api.system.scheduler.task.destroy();
+}-
